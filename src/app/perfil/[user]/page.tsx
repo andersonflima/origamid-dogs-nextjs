7f364d25b0f446af33ec28dfd7914c9f7a4cb705
Anderson Faustino Lima <andersonfaustinolima@gmail.com>
2024-10-29 11:18:47 -0300

    :bug: fix(src/app/perfil/[user]/page.tsx): PageProps
    changed params type

--- src/app/perfil/[user]/page.tsx
+++ src/app/perfil/[user]/page.tsx
@@ -1,9 +1,14 @@
 import photosGet from "@/actions/photos-get";
 import Feed from "@/components/feed/feed";
-import { PageProps } from "../../../../.next/types/app/layout";
+
+type PageProps = {
+  params: {
+    user: string;
+  };
+};
 
 export default async function PerfilUserPage({ params }: PageProps) {
-  const { user } = (await params) as { user: string };
+  const { user } = await params;
   const { data } = await photosGet({ user: user });
 
   if (!data) return null;
