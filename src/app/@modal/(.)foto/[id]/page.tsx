3eceedfdc943def05262ff7ff6fdca8644c189f1
Anderson Faustino Lima <andersonfaustinolima@gmail.com>
2024-10-29 11:18:44 -0300

    :bug: fix(src/app/@modal/(.)foto/[id]/page.tsx): PageProps
    changed params type

--- src/app/@modal/(.)foto/[id]/page.tsx
+++ src/app/@modal/(.)foto/[id]/page.tsx
@@ -1,32 +1,33 @@
 import photoGet from "@/actions/photo-get";
 import FeedModal from "@/components/feed/feed-modal";
 import { notFound } from "next/navigation";
-import { PageProps } from "../../../../../.next/types/app/layout";
 
-type FotoIdParams = {
+type PageProps = {
   params: {
     id: string;
   };
 };
 
+// Metadata generation function
 export async function generateMetadata({ params }: PageProps) {
-  const {
-    params: { id },
-  } = (await params) as FotoIdParams;
-  const { data } = await photoGet(id);
+  const { id } = await params; // Destructure `id` directly from `params`
 
-  if (!data) return { title: "Fotos" };
+  const { data } = await photoGet(id); // Fetch photo data by `id`
+
+  // Fallback metadata in case `data` is missing
   return {
-    title: data.photo.title,
+    title: data?.photo.title || "Fotos",
   };
 }
 
+// Page component function
 export default async function FotoIdPage({ params }: PageProps) {
-  const {
-    params: { id },
-  } = (await params) as FotoIdParams;
+  const { id } = await params;
   const { data } = await photoGet(id);
 
+  // Redirect to a 404 page if the photo data is not found
   if (!data) return notFound();
+
+  // Render the `FeedModal` component with the fetched photo data
   return <FeedModal photo={data} />;
 }
