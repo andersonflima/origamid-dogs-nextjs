72bdfdc5e3333c33e09fa298f6d197992cfb0d3e
Anderson Faustino Lima <andersonfaustinolima@gmail.com>
2024-10-29 10:37:50 -0300

    :bug: fix(src/app/perfil/[user]/page.tsx): PageProps
    changes params type to PageProps to work with promise

--- src/app/perfil/[user]/page.tsx
+++ src/app/perfil/[user]/page.tsx
@@ -1,18 +1,16 @@
-import photosGet from '@/actions/photos-get';
-import Feed from '@/components/feed/feed';
+import photosGet from "@/actions/photos-get";
+import Feed from "@/components/feed/feed";
+import { PageProps } from "../../../../.next/types/app/layout";
 
-export default async function PerfilUserPage({
-  params,
-}: {
-  params: { user: string };
-}) {
-  const { data } = await photosGet({ user: params.user });
+export default async function PerfilUserPage({ params }: PageProps) {
+  const { user } = (await params) as { user: string };
+  const { data } = await photosGet({ user: user });
 
   if (!data) return null;
   return (
     <section className="container mainSection">
-      <h1 className="title">{params.user}</h1>
-      <Feed photos={data} user={params.user} />
+      <h1 className="title">{user}</h1>
+      <Feed photos={data} user={user} />
     </section>
   );
 }
