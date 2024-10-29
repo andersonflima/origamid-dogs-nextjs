b111a09a17e4f0e255b13f0684819ce2db8a5b85
Anderson Faustino Lima <andersonfaustinolima@gmail.com>
2024-10-29 10:00:31 -0300

    :wrench: chore(src/): initialize project
    added files and configs

---
+++ src/app/foto/[id]/page.tsx
@@ -0,0 +1,31 @@
+import photoGet from "@/actions/photo-get";
+import PhotoContent from "@/components/photo/photo-content";
+import { notFound } from "next/navigation";
+
+type FotoIdParams = {
+  params: {
+    id: string;
+  };
+};
+
+export async function generateMetadata({ params }: FotoIdParams) {
+  const { id } = await params;
+  const { data } = await photoGet(id);
+
+  if (!data) return { title: "Fotos" };
+  return {
+    title: data.photo.title,
+  };
+}
+
+export default async function FotoIdPage({ params }: FotoIdParams) {
+  const { id } = await params;
+  const { data } = await photoGet(id);
+
+  if (!data) return notFound();
+  return (
+    <section className="container mainContainer">
+      <PhotoContent data={data} single={true} />
+    </section>
+  );
+}
