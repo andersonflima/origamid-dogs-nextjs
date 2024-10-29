b62c521c4bfa6ca75fdd45615d99426399b4fda3
Anderson Faustino Lima <andersonfaustinolima@gmail.com>
2024-10-29 10:37:45 -0300

    :bug: fix(src/actions/stats-get.ts): PageProps
    changes params type to PageProps to work with promise

--- src/actions/stats-get.ts
+++ src/actions/stats-get.ts
@@ -1,8 +1,8 @@
-'use server';
+"use server";
 
-import { STATS_GET } from '@/functions/api';
-import apiError from '@/functions/api-error';
-import { cookies } from 'next/headers';
+import { STATS_GET } from "@/functions/api";
+import apiError from "@/functions/api-error";
+import { cookies } from "next/headers";
 
 export type StatsData = {
   id: number;
@@ -12,20 +12,21 @@ export type StatsData = {
 
 export default async function statsGet() {
   try {
-    const token = cookies().get('token')?.value;
-    if (!token) throw new Error('Acesso negado.');
+    const cookieStore = await cookies();
+    const token = cookieStore.get("token")?.value;
+    if (!token) throw new Error("Acesso negado.");
     const { url } = STATS_GET();
     const response = await fetch(url, {
       headers: {
-        Authorization: 'Bearer ' + token,
+        Authorization: "Bearer " + token,
       },
       next: {
         revalidate: 60,
       },
     });
-    if (!response.ok) throw new Error('Erro ao buscar os dados.');
+    if (!response.ok) throw new Error("Erro ao buscar os dados.");
     const data = (await response.json()) as StatsData[];
-    return { data, ok: true, error: '' };
+    return { data, ok: true, error: "" };
   } catch (error) {
     return apiError(error);
   }
